e987699de0185dd7aacec5780d1deb17a9bc540a
Alexey Mironov <alemir@ciklum.com>
2020-01-17 10:47:55 +0200

    404 html and hash for routing was added

--- projects/angular-pack/src/app/app-routing.module.ts
+++ projects/angular-pack/src/app/app-routing.module.ts
@@ -44,6 +44,7 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
+      useHash: true,
       scrollPositionRestoration: 'enabled',
       preloadingStrategy: PreloadAllModules
     })
